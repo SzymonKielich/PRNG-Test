7f748e87b78dc01742e4903472879c374055d633
Gasiewicz <263668@student.pwr.edu.pl>
2025-05-27 11:12:31 +0200

    Runs test naprawiony i przeprowadzony

--- own_implementation.py
+++ own_implementation.py
@@ -84,23 +84,34 @@ def collision_test(data, m=1000):
 
     return collisions, expected_collisions
 
-def runs_test(data):
+def runs_test(data, debug=False):
     
     median = np.median(data)
     signs = [1 if x > median else 0 for x in data]
 
     runs = 1
     for i in range(1, len(signs)):
-        if signs[i] != signs[i-1]:
+        if signs[i] != signs[i - 1]:
             runs += 1
 
-    n1 = np.sum(signs)
-    n2 = len(signs) - n1
+    n1 = float(np.sum(signs))
+    n2 = float(len(signs) - n1)
+
+    if n1 == 0 or n2 == 0:
+        return np.nan, np.nan
 
     expected_runs = ((2 * n1 * n2) / (n1 + n2)) + 1
-    variance_runs = (2 * n1 * n2 * (2 * n1 * n2 - n1 - n2)) / (((n1 + n2)**2) * (n1 + n2 - 1))
+    numerator = 2 * n1 * n2 * (2 * n1 * n2 - n1 - n2)
+    denominator = ((n1 + n2) ** 2) * (n1 + n2 - 1)
+
+    if denominator == 0 or numerator < 0:
+        return np.nan, np.nan
+
+    variance_runs = numerator / denominator
+    if variance_runs <= 0:
+        return np.nan, np.nan
 
     z_stat = (runs - expected_runs) / np.sqrt(variance_runs)
     p_value = 2 * (1 - stats.norm.cdf(abs(z_stat)))
 
-    return z, p_value
+    return z_stat, p_value
